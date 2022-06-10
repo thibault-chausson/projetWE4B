92b54174cd90fcf2846860ae0b0dcd4b61dbe5ac
thibault-chausson <chausson.thibault@laposte.net>
2022-06-10 15:53:00 +0200

    menu avec information sur le type du domaine ok

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  readMore() {
+    this.router.navigate(['/', 'domaines', '1']);
+  }
 
   ngOnInit(): void {
   }
