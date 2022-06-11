3f1efd18b7d88df6fff6e3a5edc16de54f128e05
Nel <eleanore.renaud@gmail.com>
2022-06-11 15:40:11 +0200

    offf réslution du bug principale des route : en gros quand on rechargé la page le pro n'étais plus pro X)

--- src/app/menu-logged/menu-logged.component.ts
+++ src/app/menu-logged/menu-logged.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 
 @Component({
   selector: 'app-menu-logged',
@@ -8,10 +10,16 @@ import { FirebaseService } from '../services/firebase.service';
 })
 export class MenuLoggedComponent implements OnInit {
 
-  constructor(public log : FirebaseService) { }
+  isPro : boolean = false;
+  role : string | null | undefined;
+
+  constructor(public log : FirebaseService, private auth : AngularFireAuth, private db : AngularFirestore) { }
 
   ngOnInit(): void {
-    console.log(this.log.userIsPro);
+   this.role = localStorage.getItem('ROLE');
+   if(this.role == 'pro'){
+     this.isPro = true;
+   }
   }
 
   logoutFunction() {
