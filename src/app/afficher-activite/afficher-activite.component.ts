b8274c204edc6f0b1a18ad82ff248a2f82b740e1
thibault-chausson <chausson.thibault@laposte.net>
2022-06-23 12:43:40 +0200

    modifier est refait

--- src/app/afficher-activite/afficher-activite.component.ts
+++ src/app/afficher-activite/afficher-activite.component.ts
@@ -21,21 +21,14 @@ export class AfficherActiviteComponent implements OnInit {
   numeroActi : any;
 
 
-
   constructor(private activatedroute : ActivatedRoute, private db : AngularFirestore, private auth : AngularFireAuth, private fb : FirebaseService, private afficher : AffichageService) { }
   ngOnInit(): void {
     this.numeroActi = this.activatedroute.snapshot.paramMap.get('id');
     this.afficher.afficher(this.filtersLoaded, this.infoActi, this.numeroActi);
   }
 
-
-
   /*onActivite(note : number) {
     this.numeroActi = this.activatedroute.snapshot.paramMap.get('id')
     this.fb.noter(note, this.numeroActi);
   }*/
-
 }
-
-
-
